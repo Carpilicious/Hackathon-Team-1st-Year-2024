2df0b954476b66780c98bd3c1a0c8dfb7bb4b000
Carpilicious <157918064+Carpilicious@users.noreply.github.com>
2024-02-28 18:04:27 +0000

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,8 +2,6 @@ import java.io.IOException;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
 
